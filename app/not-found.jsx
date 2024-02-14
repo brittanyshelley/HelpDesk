b5d98afda823d7934d2caba7ad7164f46c891fc7
brittanyshelley <brittany.j.shelley@gmail.com>
2024-02-14 15:30:47 -0700

    Add primary color and update layout.js

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <main className="text-center">
+      <h2 className="text-3x1">Not Found</h2>
+      <p>This page does not exist</p>
+      <p>Go back to the <Link href="/">Dashboard</Link></p>
+    </main>
+  )
+}
+
